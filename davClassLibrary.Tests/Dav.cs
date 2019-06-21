f5711c1dcc9bcc5c50684fd3ab9437951fdb2885
Dav <dav2070@web.de>
2019-06-22 00:00:13 +0200

    Update the table and app ids and use session jwt for tests

--- davClassLibrary.Tests/Dav.cs
+++ davClassLibrary.Tests/Dav.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using static davClassLibrary.Models.DavUser;
 
 namespace davClassLibrary.Tests
@@ -10,11 +9,11 @@ namespace davClassLibrary.Tests
     public class Dav
     {
         public const string ProjectDirectory = @"C:\Users\dav20\source\repos\davClassLibrary\davClassLibrary.Tests";
-        public const int AppId = 12;
-        public const int TestDataTableId = 23;
-        public const int TestFileTableId = 24;
+        public const int AppId = 3;
+        public const int TestDataTableId = 3;
+        public const int TestFileTableId = 4;
         public const string ApiKey = "MhKSDyedSw8WXfLk2hkXzmElsiVStD7C8JU3KNGp";
-        public const string Jwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJuYW1lIjoiZGF2Q2xhc3NMaWJyYXJ5VGVzdFVzZXIiLCJ1c2VyX2lkIjoxMiwiZGV2X2lkIjoyLCJleHAiOjM3NTI5MTgzODQxfQ.lO-iq5UHk25wnysbrEw1PirgGBhz-rFqrK6iRGkcFnU";
+        public const string Jwt = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmNsYXNzbGlicmFyeXRlc3RAZGF2LWFwcHMudGVjaCIsInVzZXJfaWQiOjUsImRldl9pZCI6MiwiZXhwIjozNzU2MTA1MDAyMn0.jZpdLre_ZMWGN2VNbZOn2Xg51RLAT6ocGnyM38jljHI.1";
         public const string TestUserEmail = "davclasslibrarytest@dav-apps.tech";
         public const string TestUserUsername = "davClassLibraryTestUser";
         public const DavPlan TestUserPlan = DavPlan.Free;
@@ -24,10 +23,10 @@ namespace davClassLibrary.Tests
         public const string TestDataFirstTableObjectSecondPropertyValue = "Hallo Welt";
         public const string TestDataSecondTableObjectFirstPropertyValue = "Table";
         public const string TestDataSecondTableObjectSecondPropertyValue = "Tabelle";
-        public static List<int> TableIds = new List<int>{ 23, 24 };
+        public static List<int> TableIds = new List<int>{ TestDataTableId, TestFileTableId };
 
         public static TableObjectData TestDataFirstTableObject = new TableObjectData {
-            table_id = 23,
+            table_id = TestDataTableId,
             uuid = new Guid("642e6407-f357-4e03-b9c2-82f754931161"),
             properties = new Dictionary<string, string>
             {
@@ -37,7 +36,7 @@ namespace davClassLibrary.Tests
         };
         public static TableObjectData TestDataSecondTableObject = new TableObjectData
         {
-            table_id = 23,
+            table_id = TestDataTableId,
             uuid = new Guid("8d29f002-9511-407b-8289-5ebdcb5a5559"),
             properties = new Dictionary<string, string>
             {
