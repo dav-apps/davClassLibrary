9781be2a52fb19707e93fd83c4c8e34b198b896f
Dav <dav2070@web.de>
2021-03-06 23:14:41 +0100

    Add Types.cs with enums and type classes and Utils.cs with utility methods
    - Add type classes for new controller methods for returning the results of the API calls
    - Move DavPlan and DavEnvironment enums to Types.cs as Environment and Plan
    - Add utility methods for controllers for serializing json and handling API errors

---
+++ davClassLibrary/Types.cs
@@ -0,0 +1,47 @@
+﻿namespace davClassLibrary
+{
+    public enum Environment
+    {
+        Development,
+        Test,
+        Production
+    }
+
+    public enum Plan
+    {
+        Free,
+        Plus,
+        Pro
+    }
+
+    public class HttpResponse
+    {
+        public int Status { get; set; }
+        public string Data { get; set; }
+
+        public HttpResponse(int status, string data)
+        {
+            Status = status;
+            Data = data;
+        }
+    }
+
+    public class ApiResponse<T>
+    {
+        public int Status { get; set; }
+        public T Data { get; set; }
+        public ApiError[] Errors { get; set; }
+    }
+
+    public class ApiError
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+
+    internal class HandleApiErrorResult
+    {
+        public bool Success { get; set; }
+        public ApiError[] Errors { get; set; }
+    }
+}
---
+++ davClassLibrary/Utils.cs
@@ -0,0 +1,45 @@
+﻿using davClassLibrary.Controllers;
+using davClassLibrary.DataAccess;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davClassLibrary
+{
+    public static class Utils
+    {
+        public static T SerializeJson<T>(string json)
+        {
+            var serializer = new DataContractJsonSerializer(typeof(T));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            return (T)serializer.ReadObject(ms);
+        }
+
+        internal static async Task<HandleApiErrorResult> HandleApiError(string responseData)
+        {
+            var errors = SerializeJson<ApiError[]>(responseData);
+
+            if (errors.Length > 0 && errors[0].Code == ErrorCodes.AccessTokenMustBeRenewed)
+            {
+                // Renew the session
+                var renewSessionResult = await SessionsController.RenewSession(Dav.AccessToken);
+
+                if (renewSessionResult.Status == 200)
+                {
+                    // Update the access token and save it in the local settings
+                    Dav.AccessToken = renewSessionResult.Data.AccessToken;
+                    SettingsManager.SetAccessToken(Dav.AccessToken);
+
+                    return new HandleApiErrorResult { Success = true, Errors = null };
+                }
+                else
+                {
+                    return new HandleApiErrorResult { Success = false, Errors = renewSessionResult.Errors };
+                }
+            }
+
+            return new HandleApiErrorResult { Success = false, Errors = errors };
+        }
+    }
+}
