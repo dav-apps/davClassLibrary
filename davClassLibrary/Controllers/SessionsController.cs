fe0472debda8ea720ae6bf94b42a3a5e2d5204bb
Dav <dav2070@web.de>
2021-03-06 23:20:03 +0100

    Add SessionsController with RenewSession method

---
+++ davClassLibrary/Controllers/SessionsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace davClassLibrary.Controllers
+{
+    public static class SessionsController
+    {
+        public static async Task<ApiResponse<SessionResponse>> RenewSession(string accessToken)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("AUTHORIZATION", accessToken);
+
+            var response = await httpClient.GetAsync($"{Dav.ApiBaseUrl}/session/renew");
+            string responseData = await response.Content.ReadAsStringAsync();
+
+            var result = new ApiResponse<SessionResponse> { Status = (int)response.StatusCode };
+
+            if (response.IsSuccessStatusCode)
+            {
+                var sessionResponseData = Utils.SerializeJson<SessionResponseData>(responseData);
+                result.Data = sessionResponseData.ToSessionResponse();
+            }
+            else
+            {
+                var errors = Utils.SerializeJson<ApiError[]>(responseData);
+                result.Errors = errors;
+            }
+
+            return result;
+        }
+    }
+
+    public class SessionResponse
+    {
+        public string AccessToken { get; set; }
+        public string WebsiteAccessToken { get; set; }
+    }
+
+    public class SessionResponseData
+    {
+        public string access_token { get; set; }
+        public string website_access_token { get; set; }
+
+        public SessionResponse ToSessionResponse()
+        {
+            return new SessionResponse
+            {
+                AccessToken = access_token,
+                WebsiteAccessToken = website_access_token
+            };
+        }
+    }
+}
