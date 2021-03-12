f7e65e8fb725cf618ed444af464bde7a47e9a305
Dav <dav2070@web.de>
2021-03-12 21:01:20 +0100

    Remove Dav.cs, move constants to Constants and add Utils
    - Update nuget packages

---
+++ davClassLibrary.Tests/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace davClassLibrary.Tests
+{
+    internal static class Constants
+    {
+        internal const string testerXTestAppAccessToken = "ckktuu0gs00008iw3ctnrofzf";
+
+        internal const int testAppId = 5;
+        internal const int testAppFirstTestTableId = 24;
+        internal const int testAppSecondTestTableId = 25;
+
+        internal const string databaseName = "dav.db";
+    }
+}
---
+++ davClassLibrary.Tests/Utils.cs
@@ -0,0 +1,64 @@
+﻿using davClassLibrary.Common;
+using davClassLibrary.Tests.Common;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace davClassLibrary.Tests
+{
+    internal static class Utils
+    {
+        internal static void GlobalSetup()
+        {
+            ProjectInterface.LocalDataSettings = new LocalDataSettings();
+            ProjectInterface.Callbacks = new Callbacks();
+
+            Dav.Init(
+                Environment.Test,
+                Constants.testAppId,
+                new List<int> { Constants.testAppFirstTestTableId, Constants.testAppSecondTestTableId },
+                new List<int>(),
+                GetDavDataPath()
+            );
+        }
+
+        internal static async Task Setup()
+        {
+            Dav.IsLoggedIn = false;
+            Dav.AccessToken = null;
+
+            // Delete all files and folders in the test folder except the database file
+            var davFolder = new DirectoryInfo(GetDavDataPath());
+            foreach (var folder in davFolder.GetDirectories())
+                folder.Delete(true);
+
+            // Clear the database
+            var database = new davClassLibrary.DataAccess.DavDatabase();
+            await database.DropAsync();
+        }
+
+        internal static string GetDavDataPath()
+        {
+            return Directory.CreateDirectory(Path.Combine(Directory.GetCurrentDirectory(), "../../dav")).FullName;
+        }
+
+        internal static string GetProjectDirectory()
+        {
+            return Path.Combine(Directory.GetCurrentDirectory(), "../../../");
+        }
+
+        public static void ClearData()
+        {
+            DirectoryInfo directory = new DirectoryInfo(GetDavDataPath());
+            foreach (var dir in directory.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+
+            foreach (var file in directory.GetFiles())
+            {
+                file.Delete();
+            }
+        }
+    }
+}
