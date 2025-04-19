01ac8a2c27f897e421804547c05bc860719a8676
David <zurek.dav@gmail.com>
2025-04-19 22:10:48 +0200

    Rewrite UsersController to use new graphql API
    - Change GetUser method to RetrieveUser & remove GetProfileImageOfUser method

--- davClassLibrary/Controllers/UsersController.cs
+++ davClassLibrary/Controllers/UsersController.cs
@@ -1,107 +1,32 @@
-﻿using davClassLibrary.Models;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
+﻿using davClassLibrary.DataAccess;
+using davClassLibrary.Models;
+using GraphQL;
 using System.Threading.Tasks;
 
 namespace davClassLibrary.Controllers
 {
     public static class UsersController
     {
-        public static async Task<ApiResponse<User>> GetUser()
+        public static async Task<GraphQLResponse<RetrieveUserResponse>> RetrieveUser(string queryData)
         {
-            HttpResponseMessage response;
-            var httpClient = Dav.httpClient;
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Dav.AccessToken);
-
-            try
+            var retrieveUserRequest = new GraphQLRequest
             {
-                response = await httpClient.GetAsync($"{Dav.ApiBaseUrl}/user");
-            }
-            catch (Exception)
-            {
-                return new ApiResponse<User> { Success = false, Status = 0 };
-            }
-            
-            string responseData = await response.Content.ReadAsStringAsync();
-
-            var result = new ApiResponse<User>
-            {
-                Success = response.IsSuccessStatusCode,
-                Status = (int)response.StatusCode
+                OperationName = "RetrieveUser",
+                Query = $@"
+                    query RetrieveUser {{
+                        retrieveUser {{
+                            {queryData}
+                        }}
+                    }}
+                "
             };
 
-            if (response.IsSuccessStatusCode)
-            {
-                try
-                {
-                    var userData = JsonConvert.DeserializeObject<UserData>(responseData);
-                    result.Data = userData.ToUser();
-                }
-                catch (Exception)
-                {
-                    result.Success = false;
-                }
-            }
-            else
-            {
-                var errorResult = await Utils.HandleApiError(responseData);
-
-                if (errorResult.Success)
-                    return await GetUser();
-                else
-                    result.Errors = errorResult.Errors;
-            }
-
-            return result;
+            return await ApiManager.GetGraphQLClient().SendQueryAsync<RetrieveUserResponse>(retrieveUserRequest);
         }
+    }
 
-        public static async Task<ApiResponse> GetProfileImageOfUser(string filePath)
-        {
-            HttpResponseMessage response;
-            var httpClient = Dav.httpClient;
-            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Dav.AccessToken);
-
-            try
-            {
-                response = await httpClient.GetAsync($"{Dav.ApiBaseUrl}/user/profile_image");
-            }
-            catch (Exception)
-            {
-                return new ApiResponse { Success = false, Status = 0 };
-            }
-
-            var result = new ApiResponse
-            {
-                Success = response.IsSuccessStatusCode,
-                Status = (int)response.StatusCode
-            };
-
-            if (response.IsSuccessStatusCode)
-            {
-                try
-                {
-                    File.WriteAllBytes(filePath, await response.Content.ReadAsByteArrayAsync());
-                }
-                catch (Exception)
-                {
-                    result.Success = false;
-                }
-            }
-            else
-            {
-                string responseData = await response.Content.ReadAsStringAsync();
-                var errorResult = await Utils.HandleApiError(responseData);
-
-                if (errorResult.Success)
-                    return await GetProfileImageOfUser(filePath);
-                else
-                    result.Errors = errorResult.Errors;
-            }
-
-            return result;
-        }
+    public class RetrieveUserResponse
+    {
+        public UserResource RetrieveUser { get; set; }
     }
 }
